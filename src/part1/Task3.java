099c7063576756fc1fde432a30d450c6186570ef
dlogushiv <dlogushiv@gmail.com>
2018-03-24 11:35:00 +0200

    task 3 completed

---
+++ src/part1/Task3.java
@@ -0,0 +1,13 @@
+package part1;
+
+// округление вещественного числа до ближайшего целого
+public class Task3 {
+    public static void main(String[] args) {
+        double n = 6.55;
+        if (n % 1 < 0.5) {
+            System.out.println((int) n);
+        } else {
+            System.out.println((int) (n + 1));
+        }
+    }
+}
