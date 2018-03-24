be2bbbee25f4795b114838db2902b3030ff74266
dlogushiv <dlogushiv@gmail.com>
2018-03-24 11:13:49 +0200

    task 1 completed

---
+++ src/part1/Task1.java
@@ -0,0 +1,12 @@
+package part1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int q, w;
+        q = 40;
+        w = 7;
+        int a = q / w;
+        int b = q % w;
+        System.out.println(q + " / " + w + " = " + a + " and " + b + " in the remainder.");
+    }
+}
