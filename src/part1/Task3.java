09cd0c1620591b5b6886cfc01219940abe4ab17e
dlogushiv <dlogushiv@gmail.com>
2018-03-24 11:38:50 +0200

    task 3 refactoring

--- src/part1/Task3.java
+++ src/part1/Task3.java
@@ -4,10 +4,11 @@ package part1;
 public class Task3 {
     public static void main(String[] args) {
         double n = 6.55;
-        if (n % 1 < 0.5) {
-            System.out.println((int) n);
-        } else {
-            System.out.println((int) (n + 1));
-        }
+//        if (n % 1 < 0.5) {
+//            System.out.println((int) n);
+//        } else {
+//            System.out.println((int) (n + 1));
+//        }
+        System.out.println(n%1<0.5?(int)n:(int)(n+1));  // refactoring
     }
 }
