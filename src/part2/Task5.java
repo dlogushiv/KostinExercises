bb890568cdc221f84645b33e29df7f2a2b742151
dlogushiv <dlogushiv@gmail.com>
2018-04-17 23:26:32 +0300

    part 2 Task 5 completed

---
+++ src/part2/Task5.java
@@ -0,0 +1,8 @@
+package part2;
+
+// вывести веществ. пс.случ. число из [-3;3)
+public class Task5 {
+    public static void main(String[] args) {
+        System.out.println(Math.random() * 6 - 3);
+    }
+}
