9a867fbb1a6e45f50d23b0477c8493bd39dd8885
dlogushiv <dlogushiv@gmail.com>
2018-04-23 19:44:25 +0300

    part 3 Task 1 completed

---
+++ src/part3/Task1.java
@@ -0,0 +1,12 @@
+package part3;
+
+// Создать программу, проверяющую и сообщающую на экран, является ли целое число записанное в переменную n, чётным либо нечётным.
+public class Task1 {
+    public static void main(String[] args) {
+        int n = -55;
+        if (n >= 0)
+            System.out.println("n is even");
+        else
+            System.out.println("n isn't even");
+    }
+}
