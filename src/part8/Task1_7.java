761a24618de6e9265bce05269e088bbd87908725
dlogushiv <dlogushiv@gmail.com>
2018-05-10 20:43:58 +0300

    part 8 Task 1-7 completed (write on work)

--- src/part8/Task1_7.java
+++ src/part8/Task1_7.java
@@ -1,4 +1,27 @@
 package part8;
 
+// Подсчитайте, сколько раз потребуется повторно вычислить четвёртый
+// элемент последовательности Фибоначчи для вычисления пятнадцатого элемента.
+
 public class Task1_7 {
+    static int counter;
+
+    public static void main(String[] args) {
+        counter = 0;
+        int n = 15;
+        for (int i = 1; i <= n; i++) {
+            fibRec(i);
+        }
+        System.out.println(counter);
+    }
+
+    static int fibRec(int n) {
+        if (n == 4) {
+            counter++;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fibRec(n - 2) + fibRec(n - 1);
+    }
 }
